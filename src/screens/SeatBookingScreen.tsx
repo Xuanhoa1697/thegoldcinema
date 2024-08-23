d713b9f86037a203e9ae61eed7ffe07891957ae9
hoadx <hoadx@arrowhitech.com>
2024-08-24 00:46:19 +0700

    Update

--- src/screens/SeatBookingScreen.tsx
+++ src/screens/SeatBookingScreen.tsx
@@ -112,40 +112,50 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
   const [datvetruocList, setDatvetruocList] = useState([]);
   const [legendTtems, setLegendTtems] = useState([]);
   const [seats, setSeats] = useState([]);
-  const scale = useSharedValue(0.8);
+  const scale = useSharedValue(0.65);
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
-  const savedScale = useRef(0.8);
-  const savedTranslateX = useRef(0);
-  const savedTranslateY = useRef(0);
+  const savedScale = useSharedValue(0.65);
+  const savedTranslateX = useSharedValue(0);
+  const savedTranslateY = useSharedValue(0);
   const [loading, setLoading] = useState(false);
   const [selectedSeats, setSelectedSeats] = useState({});
-  const [secondsLeft, setSecondsLeft] = useState(600);
+  const [secondsLeft, setSecondsLeft] = useState(60);
 
   React.useEffect(() => {
     (async () => {
       setSelectedSeats({})
       get_seat_map_cinema_home()
     })();
+    // const intervalId = setInterval(() => {
+    //   setSecondsLeft((prevSeconds) => prevSeconds - 1);
+    // }, 1000);
+
+    // return () => {
+    //   clearInterval(intervalId);
+    //   console.log('Function đã dừng do màn hình mất focus.');
+    // };
   }, [isFocused]);
 
-  useFocusEffect(
-    useCallback(() => {
-      const intervalId = setInterval(() => {
-        setSecondsLeft((prevSeconds) => prevSeconds - 1);
-      }, 1000);
+  // useFocusEffect(
+  //   useCallback(() => {
+  //   const intervalId = setInterval(() => {
+  //     setSecondsLeft((prevSeconds) => prevSeconds - 1);
+  //   }, 1000);
 
-      return () => {
-        clearInterval(intervalId);
-        console.log('Function đã dừng do màn hình mất focus.');
-      };
-    }, [])
-  );
+  //   return () => {
+  //     clearInterval(intervalId);
+  //     console.log('Function đã dừng do màn hình mất focus.');
+  //   };
+  //   }, [])
+  // );
 
 
   const formatTime = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainingSeconds = seconds % 60;
+    // console.log(minutes, remainingSeconds);
+    
     if (minutes === 0 && remainingSeconds === 0) {
       Alert.alert('Thông báo', 'Hết thời gian thanh toán. Vui lòng chọn lại?', [
         {
@@ -209,6 +219,7 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
     }
     if (is_ghedoi) {
       let ghe_doi = seatId.split("_");
+      
       if (Number(ghe_doi[1]) % 2 === 0) {
         ghe_doi = ghe_doi[0] + "_" + (Number(ghe_doi[1]) - 1);
       } else {
@@ -216,7 +227,9 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
       }
       data_seat[[ghe_doi]] = data_seat.hasOwnProperty(ghe_doi) ? !data_seat[ghe_doi] : true;
     }
-    setSelectedSeats(data_seat);
+    const filteredByValue = Object.fromEntries(
+      Object.entries(data_seat).filter(([key, value]) => value === true) )
+    setSelectedSeats(filteredByValue);
   };
 
   const handleShowNotification = (text) => {
@@ -232,6 +245,9 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
     if (Object.keys(selectedSeats).length === 0) {
       return handleShowNotification('Vui lòng chọn ghế trước khi đặt vé');
     }
+    if (Object.keys(selectedSeats).length > 6) {
+      return handleShowNotification('Chỉ được phép đặt tối đa 6 ghế.');
+    }
     navigation.navigate('PaymentScreen', {
       detailId: detail,
       seats: selectedSeats,
@@ -241,33 +257,31 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
 
   const pinchGesture = Gesture.Pinch()
     .onStart(() => {
-      savedScale.current = scale.value;
+      savedScale.value = scale.value;
     })
     .onUpdate((event) => {
-      scale.value = savedScale.current * event.scale;
+      scale.value = savedScale.value * event.scale;
     })
     .onEnd(() => {
       if (scale.value < 1) {
-        scale.value = withTiming(0.8, { duration: 300 });
+        scale.value = withTiming(0.6, { duration: 300 });
       }
     });
 
   const panGesture = Gesture.Pan()
     .onStart(() => {
-      console.log(translateX.value, translateY.value);
-
-      savedTranslateX.current = translateX.value;
-      savedTranslateY.current = translateY.value;
+      savedTranslateX.value = translateX.value;
+      savedTranslateY.value = translateY.value;
     })
     .onUpdate((event) => {
-      // console.log(translateX, translateY);
 
-      translateX.value = savedTranslateX.current + event.translationX;
-      translateY.value = savedTranslateY.current + event.translationY;
-    }).onEnd(() => {
+      translateX.value = savedTranslateX.value + event.translationX;
+      translateY.value = savedTranslateY.value + event.translationY;
+    })
+    .onEnd(() => {
       // Thêm hiệu ứng nảy (spring) khi gesture kết thúc
-      translateX.value = withSpring(translateX.value);
-      translateY.value = withSpring(translateY.value);
+      translateX.value = translateX.value
+      translateY.value = translateY.value
     })
 
   const composedGesture = Gesture.Simultaneous(pinchGesture, panGesture);
@@ -284,143 +298,143 @@ const SeatBookingScreen = ({ navigation, route }: any) => {
 
 
   return (
-    <View style={tw`h-full w-full`}>
-      <StatusBar
-        translucent={false}
-        backgroundColor={'#000000'}
-        barStyle={'default'}
-      />
-      <View style={tw`h-[55px] w-full flex-row items-center justify-start px-2 border-b border-gray-300 absolute z-1 bg-white`}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={tw``}>
-          <MaterialIcons name="arrow-back" size={25} color={'#9c1d21'} />
-        </TouchableOpacity>
-        <View style={tw`flex items-start justify-center ml-2`}>
-          <View style={tw`flex-row items-start justify-center`}>
-            <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] font-bold text-[#9c1d21]`}>{detail.marap}</Text>
-            <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] font-bold text-black ml-1`}>{detail.rapphim.toUpperCase()}</Text>
-          </View>
-          <View style={tw`flex-row items-start justify-center`}>
-            <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] text-[#9c9c9c]`}>{detail.phong},</Text>
-            <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] text-[#9c9c9c] ml-1`}>{detail.ngaychieu.split("-")[2]}/{detail.ngaychieu.split("-")[1]}/{detail.ngaychieu.split("-")[0]}</Text>
-            {/* <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[14px] text-[#9c9c9c] ml-1`}>{detail.giobatdau} ~ {detail.ketthuc}</Text> */}
-          </View>
-        </View>
-        <Text style={tw`text-[12px] text-[#000000] absolute right-3`}>{formatTime(secondsLeft)}</Text>
-      </View>
-      <GestureHandlerRootView style={tw`h-full w-full bg-black`}>
-        <GestureDetector gesture={composedGesture}>
-          <Animated.View style={[styles.container, animatedStyle]}>
-            <Text style={tw`text-[20px] text-[#ffffff] font-bold text-center mb-15`}>MÀN HÌNH</Text>
-            <View style={[styles.containerGap20]}>
-              {loading && <View style={tw`h-full w-full absolute z-10 top-[50%] flex items-center justify-start`}>
-                <ActivityIndicator size="30" color="#ffffff" style={tw``} />
-              </View>}
-              {rowMap?.map((item, index_row) => {
-                let data_item = data[index_row].split("");
-                return (
-                  <View key={item} style={[styles.seatRow, { marginTop: -18 }]}>
-                    {columnMap?.map((subitem, index) => {
-                      if (subitem != "") {
-                        let color = '';
-                        let disableBooking = false
-                        let is_ghedoi = false
-                        if (data_item.filter((i) => i === 'a').length > 0) {
-                          color = '#3a78c3'
-                        } else {
-                          color = '#ee82ee';
-                          is_ghedoi = true
-                        }
-
-                        if (datvetruocList.includes(`${item}_${subitem}`)) {
-                          color = '#008000'
-                          disableBooking = true;
-                        }
-
-                        if (bookedSeat.includes(`${item}_${subitem}`)) {
-                          color = '#472b34';
-                          disableBooking = true
-                        }
-                        return (
-                          <TouchableOpacity
-                            key={`${item}_${subitem}`}
-                            disabled={disableBooking}
-                            style={[customStyle.hasOwnProperty(item + subitem) ? customStyle[item + subitem] : '']}
-                            onPress={() => {
-                              toggleSeat(`${item}_${subitem}`, is_ghedoi);
-                            }}>
-                            <View style={[tw`w-7 h-7 bg-white flex justify-center items-center bg-[${color}] 
-                                bg-[${(selectedSeats.hasOwnProperty(`${item}_${subitem}`) && selectedSeats[`${item}_${subitem}`]) ? '#e6cac4' : color}] 
-                              `]}>
-                              <Text style={tw`text-[11px] text-center text-white`}>{item + subitem}</Text>
-                            </View>
-                          </TouchableOpacity>
-                        );
-                      }
-
-                    })}
-                  </View>
-                );
-              })}
+      <View style={tw`h-full w-full`}>
+        <StatusBar
+          translucent={false}
+          backgroundColor={'#000000'}
+          barStyle={'default'}
+        />
+        <View style={tw`h-[55px] w-full flex-row items-center justify-start px-2 border-b border-gray-300 absolute z-1 bg-white`}>
+          <TouchableOpacity onPress={() => navigation.goBack()} style={tw``}>
+            <MaterialIcons name="arrow-back" size={25} color={'#9c1d21'} />
+          </TouchableOpacity>
+          <View style={tw`flex items-start justify-center ml-2`}>
+            <View style={tw`flex-row items-start justify-center`}>
+              <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] font-bold text-[#9c1d21]`}>{detail.marap}</Text>
+              <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] font-bold text-black ml-1`}>{detail.rapphim.toUpperCase()}</Text>
             </View>
-            <View style={[styles.seatRadioContainer, tw`mt-15`]}>
-              {legendTtems.length > 0 && <View style={styles.radioContainer}>
-                <View style={[tw`w-4 h-4 bg-white flex justify-center items-center bg-[#e6cac4]`]}>
+            <View style={tw`flex-row items-start justify-center`}>
+              <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] text-[#9c9c9c]`}>{detail.phong},</Text>
+              <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[12px] text-[#9c9c9c] ml-1`}>{detail.ngaychieu.split("-")[2]}/{detail.ngaychieu.split("-")[1]}/{detail.ngaychieu.split("-")[0]}</Text>
+              {/* <Text numberOfLines={1} ellipsizeMode='tail' style={tw`text-[14px] text-[#9c9c9c] ml-1`}>{detail.giobatdau} ~ {detail.ketthuc}</Text> */}
+            </View>
+          </View>
+          {/* <Text style={tw`text-[12px] text-[#000000] absolute right-3`}>{formatTime(secondsLeft)}</Text> */}
+        </View>
+        <GestureHandlerRootView style={tw`h-full w-full bg-black`}>
+          <GestureDetector gesture={composedGesture}>
+            <Animated.View style={[styles.container, animatedStyle, tw``]}>
+              <Text style={tw`text-[20px] text-[#ffffff] font-bold text-center mb-15`}>MÀN HÌNH</Text>
+              <View style={[styles.containerGap20]}>
+                {loading && <View style={tw`h-full w-full absolute z-10 top-[50%] flex items-center justify-start`}>
+                  <ActivityIndicator size="30" color="#ffffff" style={tw``} />
+                </View>}
+                {rowMap?.map((item, index_row) => {
+                  let data_item = data[index_row].split("");
+                  return (
+                    <View key={item} style={[styles.seatRow, { marginTop: -18 }]}>
+                      {columnMap?.map((subitem, index) => {
+                        if (subitem != "") {
+                          let color = '';
+                          let disableBooking = false
+                          let is_ghedoi = false
+                          if (data_item.filter((i) => i === 'a').length > 0) {
+                            color = '#3a78c3'
+                          } else {
+                            color = '#ee82ee';
+                            is_ghedoi = true
+                          }
 
-                </View>
-                <Text style={tw`text-[12px] text-center text-white`}>Đang chọn</Text>
-              </View>
-              }
-              {legendTtems?.map((item) => {
-                let color = '';
-                if (item[0] == 'a') {
-                  color = '#3a78c3'
-                }
-                if (item[0] == 'b') {
-                  color = '#ee82ee'
-                }
-                if (item[0] == 'g') {
-                  color = '#008000'
-                }
-                if (item[0] == 'f') {
-                  color = '#472b34'
-                }
-                return (
-                  <View style={styles.radioContainer} key={item[0]}>
-                    <View style={[tw`w-4 h-4 bg-white flex justify-center items-center bg-[${color}]`]}>
+                          if (datvetruocList.includes(`${item}_${subitem}`)) {
+                            color = '#008000'
+                            disableBooking = true;
+                          }
 
+                          if (bookedSeat.includes(`${item}_${subitem}`)) {
+                            color = '#472b34';
+                            disableBooking = true
+                          }
+                          return (
+                            <TouchableOpacity
+                              key={`${item}_${subitem}`}
+                              disabled={disableBooking}
+                              style={[customStyle.hasOwnProperty(item + subitem) ? customStyle[item + subitem] : '']}
+                              onPress={() => {
+                                toggleSeat(`${item}_${subitem}`, is_ghedoi);
+                              }}>
+                              <View style={[tw`w-7 h-7 bg-white flex justify-center items-center bg-[${color}] 
+                                  bg-[${(selectedSeats.hasOwnProperty(`${item}_${subitem}`) && selectedSeats[`${item}_${subitem}`]) ? '#e6cac4' : color}] 
+                                `]}>
+                                <Text style={tw`text-[11px] text-center text-white`}>{item + subitem}</Text>
+                              </View>
+                            </TouchableOpacity>
+                          );
+                        }
+
+                      })}
                     </View>
-                    <Text style={tw`text-[12px] text-center text-white`}>{item[2]}</Text>
+                  );
+                })}
+              </View>
+              <View style={[styles.seatRadioContainer, tw`mt-15`]}>
+                {legendTtems.length > 0 && <View style={styles.radioContainer}>
+                  <View style={[tw`w-4 h-4 bg-white flex justify-center items-center bg-[#e6cac4]`]}>
+
                   </View>
-                )
-              })}
+                  <Text style={tw`text-[12px] text-center text-white`}>Đang chọn</Text>
+                </View>
+                }
+                {legendTtems?.map((item) => {
+                  let color = '';
+                  if (item[0] == 'a') {
+                    color = '#3a78c3'
+                  }
+                  if (item[0] == 'b') {
+                    color = '#ee82ee'
+                  }
+                  if (item[0] == 'g') {
+                    color = '#008000'
+                  }
+                  if (item[0] == 'f') {
+                    color = '#472b34'
+                  }
+                  return (
+                    <View style={[styles.radioContainer, tw`ml-3`]} key={item[0]}>
+                      <View style={[tw`w-4 h-4 bg-white flex justify-center items-center bg-[${color}]`]}>
+
+                      </View>
+                      <Text style={tw`text-[12px] text-center text-white`}>{item[2]}</Text>
+                    </View>
+                  )
+                })}
+              </View>
+            </Animated.View>
+          </GestureDetector>
+        </GestureHandlerRootView>
+        <View style={tw`h-[75px] w-full flex-row items-center justify-between px-2 absolute z-1 bottom-0 bg-white`}>
+          <View style={tw`flex items-start justify-start w-full`}>
+            <Text ellipsizeMode='tail' numberOfLines={1} style={tw`font-bold text-[12px] text-black`}>{detail.phim.toUpperCase()}</Text>
+            <View style={tw`flex-row items-center justify-between w-full mt-2`}>
+              <Text style={tw`text-[#4a4a4a] text-[12px]`}>{detail.giobatdau} ~ {detail.ketthuc}</Text>
+              <TouchableOpacity onPress={BookSeats}>
+                <Text style={tw`text-white font-bold bg-[#9C1D21] text-[12px] px-8 py-2 rounded-15`}>Đặt vé</Text>
+              </TouchableOpacity>
             </View>
-          </Animated.View>
-        </GestureDetector>
-      </GestureHandlerRootView>
-      <View style={tw`h-[75px] w-full flex-row items-center justify-between px-2 absolute z-1 bottom-0 bg-white`}>
-        <View style={tw`flex items-start justify-start w-full`}>
-          <Text ellipsizeMode='tail' numberOfLines={1} style={tw`font-bold text-[12px] text-black`}>{detail.phim.toUpperCase()}</Text>
-          <View style={tw`flex-row items-center justify-between w-full mt-2`}>
-            <Text style={tw`text-[#4a4a4a] text-[12px]`}>{detail.giobatdau} ~ {detail.ketthuc}</Text>
-            <TouchableOpacity onPress={BookSeats}>
-              <Text style={tw`text-white font-bold bg-[#9C1D21] text-[12px] px-8 py-2 rounded-15`}>Đặt vé</Text>
-            </TouchableOpacity>
+
           </View>
-
         </View>
-      </View>
 
-    </View>
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    flex: 1,
+    // flex: 1,
     backgroundColor: COLORS.Black,
-    height: height - 150,
-    paddingTop: 75
+    // height: height - 150,
+    // paddingTop: 75
   },
   ImageBG: {
     width: '100%',
