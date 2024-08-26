30394d6242d092dcb8eb20ca308d689d7f770926
hoadx <hoadx@arrowhitech.com>
2024-08-26 09:09:25 +0700

    first commit

--- ios/Movie_AppTests/Movie_AppTests.m
+++ ios/Movie_AppTests/Movie_AppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface Movie_AppTests : XCTestCase
+@interface thegoldcinemaTests : XCTestCase
 
 @end
 
-@implementation Movie_AppTests
+@implementation thegoldcinemaTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
